f6aff7976c9ebb9f9700275c65d5a3acf2211946
Andrzej Tymiński <117731002+AndrzejTyminski@users.noreply.github.com>
2023-03-28 21:42:22 +0200

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleries = document.querySelector(".gallery__item");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const img = `<a class="gallery__link" href="${galleryItems[i].original}">
+  <img class="gallery__image"  
+  src="${galleryItems[i].preview}"  
+  data-source="${galleryItems[i].original}"
+  alt="${galleryItems[i].description}">`;
+  galleries.insertAdjacentHTML("afterbegin", img);
+}
+
+//************
+
+const imagePress = (e) => {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img width="90vw" height="90wh" src="${e.target.dataset.source}">`
+  );
+  instance.show();
+  const close = document.addEventListener("keydown", (e) => {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener(Object, close);
+    }
+  });
+};
+
+galleries.addEventListener("click", imagePress);
