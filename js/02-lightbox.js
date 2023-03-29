d75bbd74269f4cb1ada924f89a97854e1995ebb5
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-03-29 21:37:41 +0200

    gallery vol.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleries = document.querySelector(".gallery");
+galleries.style.gap = "20px";
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const img = `<li class="gallery__item"> 
+                    <a class="gallery__link" href="${galleryItems[i].original}">
+                            <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />
+                    </a>
+                </li>`;
+  galleries.insertAdjacentHTML("afterbegin", img);
+}
+
+//links to img full
+// const liEl = document.querySelectorAll(".gallery__link");
+// liEl.forEach((liEl) => console.log(liEl.href));
+
+//links to img small
+// const srcEl = document.querySelectorAll(".gallery__image");
+// srcEl.forEach((srcEl) => console.log(srcEl.src));
+
+new SimpleLightbox(".gallery__item a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
